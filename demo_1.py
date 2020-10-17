a6b5d1a603e6e646d1cb962f622b205606bc4b1e
TLRKiliann <philogenie@protonmail.com>
2020-10-17 20:22:07 +0200

    demo_1 finish

--- demo_1.py
+++ demo_1.py
@@ -2,47 +2,44 @@
 # -*- encoding:Utf-8 -*-
 
 
-import matplotlib.pyplot as plt
-#import seaborn as sns
+import tkinter
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
 
 
-list_date = ['01', '02', '03', '04', '05', '06', '07',
-'08', '09', '10', '11', '12', '13', '14', '15', '16', 
-'17', '18', '19', '20', '21', '22', '23', '24', '25', 
-'26', '27', '28', '29', '30', '31']
+root = tkinter.Tk()
+root.wm_title("Embedding in Tk")
 
-list_2011 = [1, -1, -3, -3, 1, 5, 7, 10, 5, 4, 3, 5, 9, 9, 8, 
-9, 7, 7, 2, -1, -3, -5, -5, -2, -1, 1, 1, 1, 2, 0, 0]
+fig = Figure(figsize=(5, 4), dpi=100)
+t = np.arange(0, 3, .01)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 
-list_2016 = [6, 5, 6, 5, 6, 5, 7, 8, 8, 8, 8, 5, 3, 4, 2, 
-0, 0, -3, 2, 3, 0, 4, 6, 7, 9, 9, 9, 10, 9, 9, 9]
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-list_2020 = [8, 8, 9, 8, 5, 9, 8, 8, 10, 8, 6, 7, 8, 9, 9, 
-10, 8, 6, 3, 3, 5, 8, 5, 8, 8, 6, 9, 8, 5, 8, 11]
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-style = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C5")
-style2 = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C6")
-style3 = dict(color="C4", linewidth=1, linestyle='--', marker='.', mec="C9")
+def on_key_press(event):
+    print("you pressed {}".format(event.key))
+    key_press_handler(event, canvas, toolbar)
 
-with plt.style.context(("seaborn-darkgrid",)):
-    fig, axes = plt.subplots(1, 3, constrained_layout=True)
-    #fig.set_size_inches(5, 5)
-    #sns.axes_style('dark')
+canvas.mpl_connect("key_press_event", on_key_press)
 
-    axes[0].plot(list_date, list_2011, **style)
-    axes[0].set_xlabel('Days of january', fontsize=12)
-    axes[0].set_ylabel('Temperatures (°C)', fontsize=12)
-    axes[0].set_title('2011')
-    axes[0].grid(color='w', linestyle='-', linewidth=1)
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
-    axes[1].plot(list_date, list_2016, **style2)
-    axes[1].set_xlabel('Days of january', fontsize=12)
-    axes[1].set_title('2016')
-    axes[1].grid(color='w', linestyle='-', linewidth=1)
+button = tkinter.Button(master=root, text="Quit", command=_quit)
+button.pack(side=tkinter.BOTTOM)
 
-    axes[2].plot(list_date, list_2020, **style3)
-    axes[2].set_xlabel('Days of january', fontsize=12)
-    axes[2].set_title('2020')
-    axes[2].grid(color='w', linestyle='-', linewidth=1)
-
-    plt.show()
+tkinter.mainloop()
+# If you put root.destroy() here, it will cause an error if the window is
+# closed with the window manager.
